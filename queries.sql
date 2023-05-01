aa55076591a13b2bffdb9456d114523db97f7d8c
jolak5 <kayodej4@gmail.com>
2023-05-01 17:28:07 +0100

    Add data to sql files

--- queries.sql
+++ queries.sql
@@ -1,5 +1,3 @@
-/*Queries that provide answers to the questions from all projects.*/
-
 SELECT * FROM animals WHERE name LIKE '%mon%'
 SELECT name FROM animals WHERE date_of_birth  BETWEEN '2016-01-01' and '2019-12-31';
 SELECT name FROM animals WHERE neutered IS true and escape_attempts < 3;
